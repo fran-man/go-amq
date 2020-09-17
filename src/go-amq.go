364bafcb38cd7176834b4b315be40ccd6d59c7ee
fran-man <fran.man@hotmail.co.uk>
2020-09-17 21:11:21 +0100

    Test to send messages to AMQ via STOMP

---
+++ src/go-amq.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/go-stomp/stomp"
+)
+
+const testSendQ string = "test.queue.2"
+const textPlain string = "text/plain"
+
+func main() {
+	con, er := stomp.Dial("tcp", "localhost:61613")
+	//defer con.Disconnect()
+	if er != nil {
+		fmt.Println("ERROR:", er)
+	} else {
+		fmt.Println("Connected successfully")
+	}
+
+	for i := 0; i < 100; i++ {
+		er = con.Send(testSendQ, textPlain, []byte("Hello World "+strconv.Itoa(i)))
+		if er != nil {
+			fmt.Println("ERROR:", er)
+		} else {
+			fmt.Println("Sent Message")
+		}
+	}
+	con.Disconnect()
+}
